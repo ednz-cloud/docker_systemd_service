562fc0215e4d7d4d30d421dd84425937924eacc0
Bertrand Lanson <bertrand.lanson@protonmail.com>
2023-12-09 18:36:25 +0100

    feat(template): move flag formatting to filter_plugin

---
+++ filter_plugins/docker_filters.py
@@ -0,0 +1,26 @@
+# filter_plugins/docker_filters.py
+
+
+def create_docker_flags(flags):
+    return "\n".join([create_docker_flag(item) for item in flags])
+
+
+def create_docker_flag(item):
+    if isinstance(item, dict):
+        key = list(item.keys())[0]
+        value = item[key]
+        if isinstance(value, list):
+            return "\n".join(['--{} "{}" \\'.format(key, val) for val in value])
+        else:
+            return '--{} "{}" \\'.format(key, value)
+    elif isinstance(item, str):
+        return "--{} \\".format(item)
+    else:
+        return ""
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            "create_docker_flags": create_docker_flags,
+        }
